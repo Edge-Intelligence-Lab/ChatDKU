6d0ee2ce40e618e54d1dff2551baec98ce42a5be
theta <theta.lin@yandex.com>
2024-05-24 14:36:07 +0800

    Add asyncio and various features to create a working crawler
    * Add checks to crawler URL validity, extension, domain; Add retries and timeout; Fix href extraction
    * Use aiohttp and asyncio for crawler; Add pdf download support
    * Add crawler retries, more file types, and delays
    * Catch TimeoutError in crawler
    * Handle other client errors in crawler
    * Cut up long filenames in crawler
    * Use Content-Type instead of extensions to detect file type in crawler

--- crawler/dku_site_crawler.py
+++ crawler/dku_site_crawler.py
@@ -1,45 +1,120 @@
+#!/usr/bin/env python3
+
 import os
-import requests
+import random
+import asyncio
+import aiohttp
+from aiohttp_retry import RetryClient, ExponentialRetry
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
-OUTPUT_ROOT = "site"
-MAX_DEPTH = 3
+OUTPUT_ROOT = "dku_website"
+MAX_DEPTH = 5
+
+# Paths of downloaded files are used to ensure uniqueness
+downloaded = set()
 
 
-def scrape_site(url, depth=0):
+def cut(path):
+    """Cut up the final part of the path into CHUNK_SIZE chunks and concatenate them all together"""
+    CHUNK_SIZE = 64
+    chunks = []
+    s = os.path.basename(path)
+    for i in range(0, len(s), CHUNK_SIZE):
+        chunks.insert(0, s[max(0, len(s) - i - CHUNK_SIZE) : len(s) - i])
+    chunks.insert(0, os.path.dirname(path))
+    return os.path.join(*chunks)
+
+
+async def scrape_site(url, depth=0):
     if depth > MAX_DEPTH:
         return
 
-    a = urlparse(url)
-    filename = os.path.join(OUTPUT_ROOT, a.path.lstrip("/"), "index.txt")
-    # Don't scrape the same page twice
-    if os.path.isfile(filename):
+    try:
+        a = urlparse(url)
+        valid = a.scheme and a.netloc
+    except AttributeError:
+        valid = False
+    if not valid:
+        print(f"Invalid URL: {url}")
         return
 
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(f"Failed to download: {url}")
+    filepath = os.path.normpath(os.path.join(OUTPUT_ROOT, a.netloc, a.path.lstrip("/")))
+    if filepath in downloaded:
+        print(f"Already downloaded: {url}")
         return
 
-    soup = BeautifulSoup(response.content, "html.parser")
-    text = soup.get_text()
+    try:
+        retry_options = ExponentialRetry(attempts=5)
+        retry_client = RetryClient(raise_for_status=False, retry_options=retry_options)
+        # XXX: Disable verification of SSL is a security risk,
+        # but some sites don't work for me if I don't do this
+        async with retry_client.get(url, verify_ssl=False) as response:
+            content_type = response.content_type.split("/")
+            status = response.status
+            if content_type[0] == "text":
+                text = await response.text()
+            else:
+                content = await response.read()
+    except aiohttp.ClientError as e:
+        print(f'Client error "{e}": {url}')
+        return
+    except asyncio.TimeoutError as e:
+        print(f'Timeout error "{e}": {url}')
+        return
+    except UnicodeDecodeError as e:
+        print(f'Decode error "{e}": {url}')
+        return
+    finally:
+        await retry_client.close()
 
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, "w") as file:
-        file.write(text)
+    if status != 200:
+        print(f"Failed {status}: {url}")
+        return
+
+    # Files with extremely long names were encountered, so they need to be shortened
+    filepath = cut(filepath)
+    # Some HTML pages were not give an explicit file name
+    if content_type[1] == "html" and not (
+        filepath.endswith(".html") or filepath.endswith(".htm")
+    ):
+        filepath = os.path.join(filepath, "index.html")
+
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    if content_type[0] == "text":
+        with open(filepath, "w") as file:
+            file.write(text)
+    else:
+        with open(filepath, "wb") as file:
+            file.write(content)
+    downloaded.add(filepath)
     print(f"Downloaded: {url}")
 
-    links = soup.find_all("a", href=True)
-    for link in links:
-        href = link.get("href")
-        # Make sure it's a relative URL
-        if href.startswith("/"):
-            absolute_url = urljoin(url, href)
-        else:
-            absolute_url = url
-        scrape_site(absolute_url, depth + 1)
+    if content_type[1] == "html" and a.netloc.endswith("dukekunshan.edu.cn"):
+        # Prevent accidentally DOSing the server
+        await asyncio.sleep(random.randint(0, 3))
+
+        soup = BeautifulSoup(text, "html.parser")
+        links = soup.find_all("a", href=True)
+
+        async with asyncio.TaskGroup() as tg:
+            for link in links:
+                href = link.get("href")
+                # Make sure it's an absolute URL
+                if href.startswith("/"):
+                    absolute_url = urljoin(url, href)
+                else:
+                    absolute_url = href
+
+                tg.create_task(scrape_site(absolute_url, depth + 1))
+                # I cannot find a way to make HTTP to HTTPS redirects work, so I'll just try both
+                if absolute_url.startswith("http://"):
+                    tg.create_task(
+                        scrape_site(
+                            "https://" + absolute_url[len("http://") :], depth + 1
+                        )
+                    )
 
 
 if __name__ == "__main__":
-    scrape_site("https://dukekunshan.edu.cn")
+    asyncio.run(scrape_site("https://www.dukekunshan.edu.cn/"))
