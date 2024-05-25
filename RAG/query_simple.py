8377702ffed13685ff23280c5c6edda68a3958c3
theta <theta.lin@yandex.com>
2024-05-25 11:20:14 +0800

    Add minimum working RAG retrieval using LlamaIndex
    * Add script for getting the count and total size of RAG_data files grouped by extension
    * Add basic load, index, store, and query with llamaindex
    * Add embedding model and disable LLM for simple RAG demo
    * Add documentation for RAG
    * Add .gitignore for chroma_db

---
+++ RAG/query_simple.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import chromadb
+from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+
+import settings  # noqa # pyright: ignore
+
+if __name__ == "__main__":
+    db = chromadb.PersistentClient(path="./chroma_db")
+    chroma_collection = db.get_or_create_collection("dku_html_pdf")
+
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, storage_context=storage_context
+    )
+
+    # TODO: Query engine could be composed from, for example, the retrieval and
+    # response synthesis components or even customized. Additionally, a query
+    # pipeline could also be established where the LLM would play a role in
+    # retrieval, reranking, and response synthesis stages.
+    query_engine = index.as_query_engine()
+
+    while True:
+        try:
+            prompt = input("> ")
+            response = query_engine.query(prompt)
+            print(response)
+        except EOFError:
+            break
