5a03b1375687392147a1dd57022eceeae5ff152c
Cody Qin <125537769+CodyQin@users.noreply.github.com>
2024-05-29 18:40:32 +0800

    venv address correction

--- RAG_groq/cookbook/llms/groq/rag/phidata/bin/activate.fish
+++ RAG_groq/cookbook/llms/groq/rag/phidata/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/codyqin/Desktop/SRS/WorkSpace/phidata/cookbook/llms/groq/rag/phidata"
+set -gx VIRTUAL_ENV "/Users/codyqin/Desktop/SRS/WorkSpace/DKU_LLM/RAG_groq/cookbook/llms/groq/rag/phidata"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
