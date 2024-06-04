892abf2844900cd4805c386a4906c61861b055ec
theta <theta.lin@yandex.com>
2024-06-04 13:53:21 +0800

    Add argument for specifying embedding model; Allow no LLM argument

--- RAG/settings.py
+++ RAG/settings.py
@@ -11,36 +11,47 @@ from pathlib import Path
 
 def parse_args_and_setup():
     parser = ArgumentParser()
-    parser.add_argument("-l", "--llm", type=Path, required=True)
+    parser.add_argument("-e", "--embedding", type=str)
+    parser.add_argument("-l", "--llm", type=Path)
     args = parser.parse_args()
 
     # TODO: Use a better embedding model
+    if args.embedding is None:
+        embedding_name = "BAAI/bge-small-en-v1.5"
+        print(f"Using default embedding {embedding_name}")
+    else:
+        embedding_name = args.embedding
+
     Settings.embed_model = HuggingFaceEmbedding(
-        model_name="BAAI/bge-small-en-v1.5", trust_remote_code=True
+        model_name=embedding_name, trust_remote_code=True
     )
     print("Loaded embedding model")
 
-    # NOTE: Arguments default to those that work with Llama3 8B, might consider adding
-    # some arguments to change these values from CLI
-    Settings.llm = LlamaCPP(
-        model_path=str(args.llm),
-        temperature=0.1,
-        max_new_tokens=256,
-        # Llama3 8B has a context window of 8192 tokens
-        context_window=8192,
-        # kwargs to pass to __call__()
-        generate_kwargs={},
-        # kwargs to pass to __init__()
-        # set to at least 1 to use GPU
-        model_kwargs={"n_gpu_layers": 1},
-        # transform inputs into Llama format
-        messages_to_prompt=messages_to_prompt,
-        completion_to_prompt=completion_to_prompt,
-        verbose=True,
-    )
-    print("Loaded LLM")
+    if args.llm is None:
+        Settings.llm = None
+        print("Not using LLM")
+    else:
+        # NOTE: Arguments default to those that work with Llama3 8B, might consider adding
+        # some arguments to change these values from CLI
+        Settings.llm = LlamaCPP(
+            model_path=str(args.llm),
+            temperature=0.1,
+            max_new_tokens=256,
+            # Llama3 8B has a context window of 8192 tokens
+            context_window=8192,
+            # kwargs to pass to __call__()
+            generate_kwargs={},
+            # kwargs to pass to __init__()
+            # set to at least 1 to use GPU
+            model_kwargs={"n_gpu_layers": 1},
+            # transform inputs into Llama format
+            messages_to_prompt=messages_to_prompt,
+            completion_to_prompt=completion_to_prompt,
+            verbose=True,
+        )
+        print("Loaded LLM")
 
-    # The same tokenizer as used by the LLM is used to count the number of tokens
-    # accurately.
-    Settings.tokenizer = Settings.llm._model.tokenizer()
-    print("Loaded tokenizer")
+        # The same tokenizer as used by the LLM is used to count the number of tokens
+        # accurately.
+        Settings.tokenizer = Settings.llm._model.tokenizer()
+        print("Loaded tokenizer")
