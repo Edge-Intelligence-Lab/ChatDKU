c4955f6d990013af8038f0f11ebf149c04e0cc69
theta <theta.lin@yandex.com>
2024-06-04 16:23:51 +0800

    Add IngestionPipeline; Refactor to make ingestion stages for configurable

--- RAG/load_and_index.py
+++ RAG/load_and_index.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-import chromadb
+import os
 import nltk
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.core import StorageContext
+import chromadb
+from llama_index.core import (
+    Settings,
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+)
 from llama_index.readers.file import UnstructuredReader
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.storage.docstore import SimpleDocumentStore
 from settings import parse_args_and_setup
 
 # Override detect_filetype so that html files containing JavaScript code are loaded in html format.
@@ -15,14 +21,19 @@ from custom_filetype_detect import custom_detect_filetype
 unstructured.file_utils.filetype.detect_filetype = custom_detect_filetype
 
 
-def main():
-    parse_args_and_setup()
-
+def load_and_index(
+    data_dir: str,
+    text_spliter: str = "sentence_splitter",
+    text_spliter_args: dict = {},
+    # NOTE: Multiprocessing appears to have issues with HuggingFaceEmbedding,
+    # please use only a single process for now.
+    pipeline_workers: int = 1,
+):
     # Required for UnstructuredReader
     nltk.download("averaged_perceptron_tagger")
     reader = UnstructuredReader()
     documents = SimpleDirectoryReader(
-        "../RAG_data",
+        data_dir,
         recursive=True,
         required_exts=[".html", ".htm", ".pdf"],
         file_extractor={
@@ -36,12 +47,38 @@ def main():
     chroma_collection = db.get_or_create_collection("dku_html_pdf")
 
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-    # TODO: Data loading could be customized either by supplying a list of custom
-    # transformations or use transformation modules explicitly. The transformation
-    # modules could be used standalone or composed in the ingestion pipeline.
-    VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+    trans = []
+    if text_spliter == "sentence_splitter":
+        from llama_index.core.node_parser import SentenceSplitter
+
+        trans.append(SentenceSplitter(**text_spliter_args))
+    else:
+        raise ValueError(f"Unsupported text_splitter: {text_spliter}")
+    trans.append(Settings.embed_model)
+
+    pipeline = IngestionPipeline(
+        transformations=trans,
+        vector_store=vector_store,
+        docstore=SimpleDocumentStore(),
+    )
+    pipeline_cache = "./pipeline_storage"
+    if os.path.exists(pipeline_cache):
+        pipeline.load(pipeline_cache)
+    pipeline.run(documents=documents, num_workers=pipeline_workers)
+    pipeline.persist(pipeline_cache)
+
+    VectorStoreIndex.from_vector_store(vector_store)
+
+
+def main():
+    parse_args_and_setup()
+    load_and_index(
+        data_dir="../RAG_data",
+        text_spliter="sentence_splitter",
+        text_spliter_args={"chunk_size": 1024, "chunk_overlap": 20},
+        pipeline_workers=1,
+    )
 
 
 if __name__ == "__main__":
