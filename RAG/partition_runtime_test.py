b982216ca7f9467605a2a1e0c37ef16c361ed357
ynyxxx <157872959+ynyxxx@users.noreply.github.com>
2024-06-06 14:39:58 +0800

    Update partition_runtime_test.py

--- RAG/partition_runtime_test.py
+++ RAG/partition_runtime_test.py
@@ -11,12 +11,17 @@ def measure_time(partition_func, file_path):
 
 def main():
     file_path = "../RAG_data/student_handbook/student_handbook_2023-08-16.pdf"
+    print(f"Original partition test start...\n")
     
     original_time = measure_time(original_partition, file_path)
-    new_time = measure_time(custom_partition, file_path)
     
-    print(f"Original partition method time: {original_time:.4f} seconds")
-    print(f"Custom partition method time: {new_time:.4f} seconds")
+    print(f"Original partition method time: {original_time:.4f} seconds\n")
+
+    print(f"Custom partition test start...\n")
+    
+    new_time = measure_time(custom_partition, file_path) 
+    
+    print(f"Custom partition method time: {new_time:.4f} seconds\n")
 
 if __name__ == "__main__":
     main()
