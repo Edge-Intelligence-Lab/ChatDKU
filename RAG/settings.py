59c110986dbc2a83ad00ada00b923922c8650638
theta <theta.lin@yandex.com>
2024-06-06 20:44:16 +0800

    Use custom prompt template to support Llama3 so there won't be weird tags in the LLM output; comment on issue about query generation

--- RAG/settings.py
+++ RAG/settings.py
@@ -1,12 +1,29 @@
 from llama_index.core import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.llama_cpp import LlamaCPP
-from llama_index.llms.llama_cpp.llama_utils import (
-    messages_to_prompt,
-    completion_to_prompt,
-)
+from llama_index.llms.llama_cpp.llama_utils import DEFAULT_SYSTEM_PROMPT
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Optional
+
+
+# NOTE: Not using `completion_to_prompt()` supplied in llama_utils as Llama 3 uses
+# a different prompt format.
+#
+# TODO: Also implement `messages_to_prompt()` for Llama 3. However, it is used for
+# `llm.chat()` as opposed to `llm.complete()`, which is used only in a few places
+# such as agents. So this might not be an urgent task. Also consider opening a PR
+# to contribute back to LlamaIndex?
+def completion_to_prompt(completion: str, system_prompt: Optional[str] = None) -> str:
+    system_prompt_str = system_prompt or DEFAULT_SYSTEM_PROMPT
+
+    return (
+        f"<|start_header_id|>system<|end_header_id|>\n\n"
+        f"{system_prompt_str.strip()}<|eot_id|>"
+        f"<|start_header_id|>user<|end_header_id|>\n\n"
+        f"{completion.strip()}<|eot_id|>"
+        f"<|start_header_id|>assistant<|end_header_id|>\n\n"
+    )
 
 
 def parse_args_and_setup():
@@ -45,7 +62,7 @@ def parse_args_and_setup():
             # set to at least 1 to use GPU
             model_kwargs={"n_gpu_layers": 1},
             # transform inputs into Llama format
-            messages_to_prompt=messages_to_prompt,
+            messages_to_prompt=None,
             completion_to_prompt=completion_to_prompt,
             verbose=True,
         )
