52e6177be93cb5be4b88919923786de0a68d051b
ynyxxx <ynyxxx@github.com/ynyxxx/DKU_LLM>
2024-06-09 12:19:38 +0000

    Decoupling of data read-in and load-indexing improves test efficiency and tunability

--- RAG/load_and_index.py
+++ RAG/load_and_index.py
@@ -1,18 +1,16 @@
-#!/usr/bin/env python3
-
 import os
-import argparse
 import nltk
 import chromadb
 from llama_index.core import (
     Settings,
-    SimpleDirectoryReader,
     VectorStoreIndex,
+    Document
 )
 from llama_index.readers.file import UnstructuredReader
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core.storage.docstore import SimpleDocumentStore
 from llama_index.core.ingestion import IngestionPipeline
+from pathlib import Path
 from settings import parse_args_and_setup
 
 # Override detect_filetype so that html files containing JavaScript code are loaded in html format.
@@ -25,40 +23,49 @@ unstructured.file_utils.filetype.detect_filetype = custom_detect_filetype
 import unstructured.partition.auto
 from custom_partation import partition
 
-unstructured.partition.auto.partition = partition
+#unstructured.partition.auto.partition = partition
 
 
-def load_documents(data_dir: str, output_dir: str):
-    # Required for UnstructuredReader
+def extract_and_save_documents(data_dir, reader, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    
+    for root, dirs, files in os.walk(data_dir):
+        for file in files:
+            file_path = Path(root) / file
+            if file_path.suffix in [".html", ".htm", ".pdf", ".csv"]:
+                documents = reader.load_data(file=file_path, split_documents=True)
+                for i, doc in enumerate(documents):
+                    output_file_path = Path(output_dir) / (file + f"_{i}.txt")
+                    with open(output_file_path, 'w', encoding='utf-8') as text_file:
+                        text_file.write(doc.text)
+
+
+def load_and_index(data_dir, text_spliter, text_spliter_args, pipeline_workers,output_dir):
     nltk.download("averaged_perceptron_tagger")
     reader = UnstructuredReader()
-    documents = SimpleDirectoryReader(
-        data_dir,
-        recursive=True,
-        required_exts=[".html", ".htm", ".pdf", ".csv"],
-        file_extractor={
-            ".htm": reader,
-            ".html": reader,
-            ".pdf": reader,
-            ".csv": reader,
-        },
-    ).load_data()
-    
-    # Save documents to output directory
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    for i, document in enumerate(documents):
-        with open(os.path.join(output_dir, f"document_{i}.txt"), "w") as f:
-            f.write(document)
 
-def index_documents(document_dir: str, text_spliter: str = "sentence_splitter", text_spliter_args: dict = {}, pipeline_workers: int = 1):
+    # Extract and save the documents using UnstructuredReader
+    if not os.path.exists(output_dir):
+        print("Create text_documents")
+        extract_and_save_documents(data_dir, reader,output_dir)
+
+    # Load documents from saved text files
+    documents = []
+    for root,dirs,files in os.walk(output_dir):
+        for file_name in files:
+            file_path = os.path.join(root, file_name) 
+            with open(file_path, 'r', encoding='utf-8') as file: 
+                content = file.read()
+                doc_id = os.path.splitext(os.path.basename(file_path))[0]
+                documents.append(Document(text=content, id=doc_id))
+
     trans = []
     if text_spliter == "sentence_splitter":
         from llama_index.core.node_parser import SentenceSplitter
-
         trans.append(SentenceSplitter(**text_spliter_args))
     else:
         raise ValueError(f"Unsupported text_splitter: {text_spliter}")
+
     trans.append(Settings.embed_model)
 
     db = chromadb.PersistentClient(path="./chroma_db")
@@ -67,11 +74,6 @@ def index_documents(document_dir: str, text_spliter: str = "sentence_splitter", 
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
     docstore = SimpleDocumentStore()
 
-    documents = []
-    for filename in os.listdir(document_dir):
-        with open(os.path.join(document_dir, filename), "r") as f:
-            documents.append(f.read())
-    
     pipeline = IngestionPipeline(
         transformations=trans,
         vector_store=vector_store,
@@ -87,21 +89,20 @@ def index_documents(document_dir: str, text_spliter: str = "sentence_splitter", 
     docstore.persist("./docstore")
 
 
+
 def main():
     parse_args_and_setup()
-    parser = argparse.ArgumentParser(description="Document loader and indexer")
-    parser.add_argument('--load', action='store_true', help='Load documents before indexing')
-    args = parser.parse_args()
-
-    document_dir = "./loaded_documents"
-    if args.load or not os.listdir(document_dir):  # Load documents if --load is specified or if the directory is empty
-        load_documents(data_dir="../RAG_data", output_dir=document_dir)
-    index_documents(
-        document_dir=document_dir,
+    output_dir = "./extracted_docs" 
+    load_and_index(
+        data_dir="../RAG_data",
         text_spliter="sentence_splitter",
         text_spliter_args={"chunk_size": 1024, "chunk_overlap": 20},
         pipeline_workers=1,
+        output_dir=output_dir,
     )
+
+
 
 if __name__ == "__main__":
     main()
+
