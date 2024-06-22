7c49c749c7cbcd6bc5d14688920df74cce712c06
theta <theta.lin@yandex.com>
2024-06-22 15:23:22 +0800

    Fix that force update argument is always set
    * Fix typing hints
    * Better clarify the default arguments
    * Fix formatting
    * Fix wrong use of lower-case any as typing hint

--- RAG/update_data.py
+++ RAG/update_data.py
@@ -20,9 +20,10 @@ unstructured.partition.auto.partition = partition
 
 import hashlib
 
+
 def hash_file(filename):
     h = hashlib.sha256()
-    with open(filename, 'rb') as file:
+    with open(filename, "rb") as file:
         while True:
             chunk = file.read(h.block_size)
             if not chunk:
@@ -30,21 +31,22 @@ def hash_file(filename):
             h.update(chunk)
     return h.hexdigest()
 
+
 def hash_directory(directory):
-    all_hashes = ''
+    all_hashes = ""
     for root, _, files in os.walk(directory):
         for filename in files:
             filepath = os.path.join(root, filename)
             file_hash = hash_file(filepath)
             all_hashes += file_hash
-    final_hash = hashlib.sha256(all_hashes.encode('utf-8')).hexdigest()
+    final_hash = hashlib.sha256(all_hashes.encode("utf-8")).hexdigest()
     return final_hash
 
+
 def update_data(data_dir=None):
-    
     if data_dir is None:
-        data_dir=Setting.data_dir
-    
+        data_dir = Setting.data_dir
+
     # Required for UnstructuredReader
     nltk.download("averaged_perceptron_tagger")
     reader = UnstructuredReader()
@@ -63,28 +65,28 @@ def update_data(data_dir=None):
             ".csv": reader,
         },
     ).load_data()
+
     with open(documents_path, "wb") as f:
         pickle.dump(documents, f)
-
-    print("Length of documents:",len(documents))
+    print(f"Documents stored in {documents_path}")
+    print("Length of documents:", len(documents))
     return documents
 
+
 def main(data_dir=None):
-    
     if data_dir is None:
-        data_dir=Setting.data_dir
+        data_dir = Setting.data_dir
 
     update_data(data_dir)
-    hash=hash_directory(data_dir)
-    hash_path= os.path.join("./","hash.pkl")
+    hash = hash_directory(data_dir)
+    hash_path = os.path.join("./", "hash.pkl")
     with open(hash_path, "wb") as hf:
-        pickle.dump(hash,hf)
+        pickle.dump(hash, hf)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process data directory path")
-    parser.add_argument('data_dir', type=str, help='The directory containing the data')
+    parser.add_argument("data_dir", type=str, help="The directory containing the data")
     args = parser.parse_args()
 
     main(args.data_dir)
-
-
