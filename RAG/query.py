d72b8baff9b8a4e3d15ea421640ba9136e8744d6
theta-lin <theta.lin@yandex.com>
2024-07-01 21:37:15 +0800

    Use RouterRetriever to select which retriever to use

--- RAG/query.py
+++ RAG/query.py
@@ -5,10 +5,15 @@ from llama_index.core import VectorStoreIndex, get_response_synthesizer
 import chromadb
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core.storage.docstore import SimpleDocumentStore
-from llama_index.core.indices.query.query_transform import HyDEQueryTransform
+from llama_index.core.tools import RetrieverTool
+from llama_index.core.selectors import LLMSingleSelector
+
+# from llama_index.core.indices.query.query_transform import HyDEQueryTransform
 from llama_index.retrievers.bm25 import BM25Retriever
-from llama_index.core.retrievers import QueryFusionRetriever, TransformRetriever
-from llama_index.core.retrievers.fusion_retriever import FUSION_MODES
+from llama_index.core.retrievers import RouterRetriever
+
+# from llama_index.core.retrievers import QueryFusionRetriever, TransformRetriever
+# from llama_index.core.retrievers.fusion_retriever import FUSION_MODES
 from llama_index.core.response_synthesizers import ResponseMode
 from llama_index.core.query_pipeline import QueryPipeline, InputComponent
 
@@ -23,13 +28,14 @@ from settings import Config, get_parser, setup
 
 
 def get_pipeline(
-    retriever_type: str = "fusion",
-    hyde: bool = True,
-    vector_top_k: int = 5,
-    bm25_top_k: int = 5,
-    fusion_top_k: int = 5,
-    fusion_mode: FUSION_MODES = FUSION_MODES.RECIPROCAL_RANK,
-    num_queries: int = 3,
+    # retriever_type: str = "fusion",
+    # hyde: bool = True,
+    # vector_top_k: int = 5,
+    # bm25_top_k: int = 5,
+    # fusion_top_k: int = 5,
+    # fusion_mode: FUSION_MODES = FUSION_MODES.RECIPROCAL_RANK,
+    # num_queries: int = 3,
+    top_k: int = 5,
     synthesize_response: bool = True,
     response_mode: ResponseMode = ResponseMode.COMPACT,
 ) -> QueryPipeline:
@@ -65,43 +71,59 @@ def get_pipeline(
     chroma_collection = db.get_or_create_collection("dku_html_pdf")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
     index = VectorStoreIndex.from_vector_store(vector_store)
-    vector_retriever = index.as_retriever(similarity_top_k=vector_top_k)
+    vector_retriever = index.as_retriever(similarity_top_k=top_k)
 
-    if hyde:
-        # NOTE: `HyDEQueryTransform` would effectively not work if used as an
-        # component of the query pipeline by itself, since it returns a `QueryBundle`
-        # with custom embedding strings that would be dropped when passed down the
-        # pipeline as only the `query_str` attribute would be sent to the next
-        # component.
-        vector_retriever = TransformRetriever(
-            retriever=vector_retriever,
-            query_transform=HyDEQueryTransform(include_original=True),
-        )
+    # if hyde:
+    #     # NOTE: `HyDEQueryTransform` would effectively not work if used as an
+    #     # component of the query pipeline by itself, since it returns a `QueryBundle`
+    #     # with custom embedding strings that would be dropped when passed down the
+    #     # pipeline as only the `query_str` attribute would be sent to the next
+    #     # component.
+    #     vector_retriever = TransformRetriever(
+    #         retriever=vector_retriever,
+    #         query_transform=HyDEQueryTransform(include_original=True),
+    #     )
 
-    if retriever_type == "vector":
-        retriever = vector_retriever
+    # if retriever_type == "vector":
+    #     retriever = vector_retriever
 
-    elif retriever_type == "fusion":
-        docstore = SimpleDocumentStore.from_persist_path(Config.docstore_path)
-        bm25_retriever = BM25Retriever.from_defaults(
-            docstore=docstore, similarity_top_k=bm25_top_k
-        )
+    # elif retriever_type == "fusion":
+    docstore = SimpleDocumentStore.from_persist_path(Config.docstore_path)
+    bm25_retriever = BM25Retriever.from_defaults(
+        docstore=docstore, similarity_top_k=top_k
+    )
 
-        # NOTE: I am not sure why, but when using this retriever you MUST supply an LLM,
-        # otherwise errors will be reported at the synthesizer stage. While this might
-        # be due to the need of using an LLM at the query generation stage, it still
-        # won't work if you set num_queries=1.
-        retriever = QueryFusionRetriever(
-            [vector_retriever, bm25_retriever],
-            similarity_top_k=fusion_top_k,
-            mode=fusion_mode,
-            num_queries=num_queries,
-            use_async=True,
-            verbose=True,
-        )
+    # NOTE: I am not sure why, but when using this retriever you MUST supply an LLM,
+    # otherwise errors will be reported at the synthesizer stage. While this might
+    # be due to the need of using an LLM at the query generation stage, it still
+    # won't work if you set num_queries=1.
+    # retriever = QueryFusionRetriever(
+    #     [vector_retriever, bm25_retriever],
+    #     similarity_top_k=fusion_top_k,
+    #     mode=fusion_mode,
+    #     num_queries=num_queries,
+    #     use_async=True,
+    #     verbose=True,
+    # )
 
-    else:
-        raise ValueError(f"Unsupported retriever_type: {retriever_type}")
+    # else:
+    #     raise ValueError(f"Unsupported retriever_type: {retriever_type}")
+
+    vector_tool = RetrieverTool.from_defaults(
+        retriever=vector_retriever,
+        description="Retrieve texts semantically similar to the query.",
+    )
+    keyword_tool = RetrieverTool.from_defaults(
+        retriever=bm25_retriever,
+        description="Retrieve texts that contain the same keywords given in the query.",
+    )
+    retriever = RouterRetriever(
+        selector=LLMSingleSelector.from_defaults(),
+        retriever_tools=[
+            vector_tool,
+            keyword_tool,
+        ],
+    )
 
     pipeline = QueryPipeline(verbose=True)
     pipeline.add_modules(
@@ -142,13 +164,13 @@ def main():
     LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
 
     pipeline = get_pipeline(
-        retriever_type="fusion",
-        hyde=True,
-        vector_top_k=5,
-        bm25_top_k=5,
-        fusion_top_k=5,
-        fusion_mode=FUSION_MODES.RECIPROCAL_RANK,
-        num_queries=3,
+        # retriever_type="fusion",
+        #  hyde=True,
+        #  vector_top_k=5,
+        #  bm25_top_k=5,
+        #  fusion_top_k=5,
+        #  fusion_mode=FUSION_MODES.RECIPROCAL_RANK,
+        #  num_queries=3,
         synthesize_response=True,
         response_mode=ResponseMode.COMPACT,
     )
