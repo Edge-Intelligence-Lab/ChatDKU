f947489732360a80227ab40d88c3132cd7ae1028
theta <theta.lin@yandex.com>
2024-07-08 16:32:14 +0800

    Update config

--- RAG/config.py
+++ RAG/config.py
@@ -6,16 +6,17 @@ class Config:
         self.llm = "meta-llama/Meta-Llama-3-8B-Instruct"
         self.ollama_url = "http://localhost:11434"
         self.llm_url = "http://localhost:8000/v1"
-        
+
         # about load_and_index
-        self.data_dir = "/home/Glitterccc/projects/DKU_LLM/RAG_data"
-        self.documents_path = "eng_documents.pkl"
+        self.data_dir = "/opt/RAG_data"
+        self.documents_path = "/opt/RAG_data/eng_documents.pkl"
         self.update = False
 
         # about query
-        self.chroma_db = f"./chroma_dbs/eng_{str(embedding_model_type)}_bge_chroma_db"
-        self.nodes_path = f"./nodes/nodes_{str(embedding_model_type)}_bge.pkl"
-        self.docstore_path = f"./docstores/eng_{str(embedding_model_type)}_bge_docstore"
-        
-
-
+        self.chroma_db = (
+            f"/opt/chroma_dbs/eng_{str(embedding_model_type)}_bge_chroma_db"
+        )
+        # self.nodes_path = f"./nodes/nodes_{str(embedding_model_type)}_bge.pkl"
+        self.docstore_path = (
+            f"/opt/docstores/eng_{str(embedding_model_type)}_bge_docstore"
+        )
