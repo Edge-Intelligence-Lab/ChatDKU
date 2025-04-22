96468cb73234517fac26f1c371d89c35efd3feea
Munish Lohani <munish.lohani@gmail.com>
2025-04-22 20:21:05 +0800

    admin view setup

--- chatdku/chatdku/backend/extentions.py
+++ chatdku/chatdku/backend/extentions.py
@@ -4,5 +4,5 @@ from flask_admin import Admin
 
 db = SQLAlchemy()
 migrate = Migrate()
-admin=Admin()
+admin=Admin(name="Dashboard", template_mode="bootstrap4")
 # A different file to avoid circular imports. Future updates can include structuring the backend using methods or flask blueprints
