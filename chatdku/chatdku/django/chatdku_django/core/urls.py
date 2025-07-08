b4478cae824c6c81be6a36c61dbef75e7f843de0
munishlohani <munish.lohani@gmail.com>
2025-07-08 14:28:32 +0000

    urls update

--- chatdku/chatdku/django/chatdku_django/core/urls.py
+++ chatdku/chatdku/django/chatdku_django/core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns=[
-    path("upload",views.upload,name="upload")
+    path("upload",views.upload,name="upload"),
+    path("user_files",views.get_user_files,name="get_user_files")
 ]
