1f4d3a015f875d5d93c1f1a1b9c3b0c6cf1b2e7f
Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
2025-08-26 17:53:29 +0000

    fixed the prompt length limit problem

--- chatdku/chatdku/core/dspy_classes/tool_memory.py
+++ chatdku/chatdku/core/dspy_classes/tool_memory.py
@@ -167,10 +167,10 @@ class ToolMemory(dspy.Module):
                 self.summary = self.compressor(**compressor_inputs).current_summary
                 self.history = self.history[min_index:]
 
-            print("\n\n\n\n\n")
-            print(type(self.history))
-            print(self.history)
-            print("\n\n\n\n\n")
+            # print("\n\n\n\n\n")
+            # print(type(self.history))
+            # print(self.history)
+            # print("\n\n\n\n\n")
             span.set_attributes(
                 {
                     SpanAttributes.OUTPUT_VALUE: safe_json_dumps(
