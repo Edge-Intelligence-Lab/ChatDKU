703d40e3541a8ff4b87ab7b122398df7832bb16c
zhiwei531 <zl457@duke.edu>
2025-11-21 05:50:50 +0000

    update redis listenr

--- chatdku/chatdku/core/tools/redis_listener/redis_listener.py
+++ chatdku/chatdku/core/tools/redis_listener/redis_listener.py
@@ -1,16 +1,18 @@
+import dotenv
+dotenv.load_dotenv()
+
 import os
 import sys
 import time
 import json
 import logging
 import redis
-import dotenv
 import threading
-from datetime import datetime, timedelta
+from datetime import datetime
+
 from ChatDKU.chatdku.chatdku.config import config
 from ChatDKU.chatdku.chatdku.core.tools.email.email_tool import EmailTools  
 
-dotenv.load_dotenv()
 
 # ------------ Redis 连接配置 ------------
 REDIS_HOST = config.redis_host
