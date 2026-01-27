ef956d673cec18371bc612e00bfd67ff00f55813
munishlohani <munish.lohani@gmail.com>
2026-01-27 06:29:16 +0000

    email retrieval from user model

--- chatdku/chatdku/django/chatdku_django/core/utils.py
+++ chatdku/chatdku/django/chatdku_django/core/utils.py
@@ -13,7 +13,7 @@ def slugify(name: str) -> str:
 
 
 def get_admin_email():
-    admin_emails=list(User.objects.filter(email__isnull=False).exclude(email=""))
-    return admin_emails
+    admin_emails=list(User.objects.filter(email__isnull=False).exclude(email="").values_list("email", flat=True))
+    return str(admin_emails)
 
 
